34737b9b6747eff5f5646a0167c77cefadd9c247
schwarzm3283 <schwarz.mike@gmail.com>
2017-05-22 21:55:49 -0400

    update

--- Django-Python-Full-Stack-Web-Devloper-master/Python_Level_One/Part3_Lists.py
+++ Django-Python-Full-Stack-Web-Devloper-master/Python_Level_One/Part3_Lists.py
@@ -1,8 +1,9 @@
-# # Lists
+# Lists
 #
 # Earlier when discussing strings we introduced the concept of a "sequence" in
 # Python. Lists can be thought of the most general version of a "sequence" in
-# Python. Unlike strings, they are mutable, meaning the elements inside a list can be changed!
+# Python. Unlike strings, they are mutable,
+# meaning the elements inside a list can be changed!
 #
 # In this section we will learn about:
 #
@@ -12,18 +13,19 @@
 #     4.) Nesting Lists
 #     5.) Introduction to List Comprehensions
 #
-# Lists are constructed with brackets [] and commas separating every element in the list.
+# Lists are constructed with brackets []
+# and commas separating every element in the list.
 #
 # Let's go ahead and see how we can construct lists!
 
 # Assign a list to an variable named my_list
-my_list = [1,2,3]
+my_list = [1, 2, 3]
 
 
 # We just created a list of integers, but lists can actually
 # hold different object types. For example:
 
-my_list = ['A string',23,100.232,'o']
+my_list = ['A string', 23, 100.232, 'o']
 
 
 # Just like strings, the len() function will tell you how
@@ -32,12 +34,12 @@ len(my_list)
 
 
 ##############################
-#### Indexing and Slicing ####
+# Indexing and Slicing ####
 ##############################
 
 # Indexing and slicing works just like in strings. Let's make a new list to
 # remind ourselves of how this works:
-my_list = ['one','two','three',4,5]
+my_list = ['one', 'two', 'three', 4, 5]
 
 # Grab element at index 0
 my_list[0]
