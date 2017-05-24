932300bb7a78a5305f357729954bd2c9c469bd9a
schwarzm3283 <schwarz.mike@gmail.com>
2017-05-24 17:31:08 -0400

    update

---
+++ Django-Python-Full-Stack-Web-Devloper-master/JavaScript_Level_One/es1.js
@@ -0,0 +1,11 @@
+// imput weight in lbs
+var lbs = prompt("what is the weight");
+
+// convert to kilos
+kilos = lbs * 0.454;
+
+// display conversion
+alert("that is " + kilos + " kilograms");
+
+// Output to log conversion complete
+console.log("Conversion Complete");
